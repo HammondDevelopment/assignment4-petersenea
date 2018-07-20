458d3f67b54c8ae70b0cdb44c4835b8e5cbc823d
Finley Lau <finleylau98@gmail.com>
2018-07-20 10:08:35 -0500

    starter code

---
+++ assignment4.py
@@ -0,0 +1,15 @@
+def add(x,y):
+  # TODO: Return the sum of x and y
+  return NotImplementedError
+  
+def subtract(x,y):
+  # TODO: Return the difference of x and y
+  return NotImplementedError
+
+def multiply(x, y):
+  # TODO: Return the product of x and y
+  return NotImplementedError
+
+def divide(x, y):
+  # TODO: Return the quotient of x and y
+  return NotImplementedError
